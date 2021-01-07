880989ed8fdafdf2cb2cef451a80dad3f193fd7d
Reinier van der Hoeven <reinier.vanderhoeven@appythings.nl>
2021-01-07 10:23:30 +0100

    added yaml support for specs api

--- src/portal.js
+++ src/portal.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 const Apigee = require('./lib/apigee')
+const yaml = require('js-yaml')
 const fs = require('fs')
+
+const readSwaggerFile = (swaggerFile) => {
+  const swagger = fs.readFileSync(swaggerFile, 'utf8')
+
+  if (swaggerFile.endsWith('.yml') || swaggerFile.endsWith('.yaml')) {
+    return yaml.safeLoad(swagger)
+  }
+  if (swaggerFile.endsWith('.json')) {
+    return JSON.parse(swagger)
+  }
+  throw new Error('Openapi spec must be either yaml/yml or json')
+}
 
 module.exports = async (config, spec, product, portal) => {
   const apigee = new Apigee(config)
   await apigee.login()
-  const swagger = JSON.parse(fs.readFileSync(spec))
+  const swagger = readSwaggerFile(spec)
 
   const specification = await apigee.spec.createOrUpdateSwagger(swagger)
   await apigee.portal.publishSpecToPortal(swagger, specification, product, portal)
