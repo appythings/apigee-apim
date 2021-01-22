36f78d069c22fb119b515370165661aa1cc76ad9
Reinier van der Hoeven <reinier.vanderhoeven@appythings.nl>
2021-01-22 09:58:16 +0100

    added license

--- src/listapiproducts.js
+++ src/listapiproducts.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 const Apigee = require('./lib/apigee')
-const yaml = require('js-yaml')
-const fs = require('fs')
 
-module.exports = async (config, manifest) => {
+module.exports = async (config) => {
   const apigee = new Apigee(config)
   const products = await apigee.apiproduct.list()
   console.log(JSON.stringify(products));
