a0910c47d42271f35a5bd9f4c0cfa00132056433
webbeheer-unitsol <webbeheer-unitsol.nl>
2022-02-21 16:47:52 +0100

    2min timeout

--- src/lib/apigee.js
+++ src/lib/apigee.js
@@ -25,7 +25,7 @@ class Apigee {
       /*httpsAgent: HttpAgent,*/
       proxy: false,
       baseURL: config.url,
-      timeout: 240000,
+      timeout: 120000,
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
