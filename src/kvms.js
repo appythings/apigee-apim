4a6320fda06bdb5eeca49ae32d871f997d35fb90
jnbalker <96864867+jnbalker@users.noreply.github.com>
2022-03-03 09:36:01 +0100

    If installation is hybrid edit newkvm object.

--- src/kvms.js
+++ src/kvms.js
@@ -40,17 +40,14 @@ module.exports = async (config, manifest, purgeDeleted) => {
     const encrypted = kvm.encrypted === 'true' || kvm.encrypted === true
     delete kvm.encrypted
 
+    const newKvm = {
+      'name': kvmName,
+      'encrypted': encrypted,
+      'entry': Object.keys(kvm).map(key => ({ 'name': key, 'value': getValue(kvm[key]) }))
+    }
     if (apigee.config.hybrid) {
-      let newkvm = {
-        'name': kvmName,
-        'encrypted': true
-      }
-    } else {  
-      let newkvm = {
-        'name': kvmName,
-        'encrypted': encrypted,
-        'entry': Object.keys(kvm).map(key => ({ 'name': key, 'value': getValue(kvm[key]) }))
-      }
+      newkvm.encrypted = true
+      delete newkvm['entry']
     }
 
     try {
