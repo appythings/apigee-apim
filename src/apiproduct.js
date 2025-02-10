6878adf6e6a1eb2ffc9e8d719277e3064728c523
reinier.vanderhoeven <reinier.vanderhoeven@appythings.nl>
2025-02-10 16:07:43 +0100

    v0.1.42

--- src/apiproduct.js
+++ src/apiproduct.js
@@ -3,16 +3,6 @@ const Apigee = require('./lib/apigee')
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-const isUpdated = (a, b, properties) => {
-  return properties.find(prop => {
-    if (prop.includes('.')) {
-      const props = prop.split('.')
-      return a[props[0]][props[1]] !== b[props[0]][props[1]]
-    }
-    return a[prop] !== b[prop]
-  })
-}
-
 module.exports = async (config, manifest) => {
   const apigee = new Apigee(config)
   let yml = yaml.safeLoad(fs.readFileSync(manifest, 'utf8'))
@@ -24,6 +14,52 @@ module.exports = async (config, manifest) => {
     return false
   }
   return productConfig.map(async (product) => {
+    const operations = []
+    if (product.openapi) {
+      const openapi = JSON.parse(fs.readFileSync(product.openapi, 'utf8'));
+      const paths = Object.keys(openapi.paths)
+      if (product.basePath) {
+        paths.map(path => {
+          operations.push(
+            {
+              "apiSource": product.apiSource,
+              "operations": [{
+                "resource": path.replace(product.basePath, ''),
+                "methods": Object.keys(openapi.paths[path]).map(method => method.toUpperCase())
+              }]
+            })
+        })
+      }
+
+      const newProduct = {
+        'approvalType': product.approvalType || 'auto',
+        'attributes': [
+          {
+            'name': 'access',
+            'value': product.accessType || 'public'
+          }
+        ],
+        'description': product.description,
+        'displayName': product.displayName || product.name,
+        'environments': product.environments,
+        "quota": product.quota,
+        "quotaInterval": product.quotaInterval,
+        "quotaTimeUnit": product.quotaTimeUnit,
+        'name': product.name,
+        "operationGroup": {
+          "operationConfigs": operations
+        },
+        'scopes': []
+      }
+      if (product.tags) {
+        newProduct.attributes.push({
+          'name': 'tags',
+          'value': product.tags
+        })
+      }
+
+      return apigee.apiproduct.add(newProduct)
+    }
     const newProduct = {
       'apiResources': [],
       'approvalType': product.approvalType || 'auto',
@@ -34,7 +70,7 @@ module.exports = async (config, manifest) => {
         }
       ],
       'description': product.description,
-      'displayName': product.name,
+      'displayName': product.displayName || product.name,
       'environments': product.environments,
       "quota": product.quota,
       "quotaInterval": product.quotaInterval,
